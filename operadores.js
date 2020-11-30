2f5e2282a3f320f3b4d0c298cef7486e405843fa
Engelbert Paiva <engelbert.paiva@gmail.com>
2020-11-29 21:52:26 -0300

    inclusão dos codigos em java script do curso gama academy

---
+++ operadores.js
@@ -0,0 +1,7 @@
+var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando;
+
+console.log(resultado);
+
---
+++ teste.js
@@ -0,0 +1,8 @@
+var nome = "Engelbert";
+
+if (nome === "Engelbert") {
+    console.log("Muito bem seu nome é esse mesmo");
+}
+else {
+    console.log("O seu nome não é esse");
+}
