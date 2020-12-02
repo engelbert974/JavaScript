e6369042df8e701a1730b278e777185955c7077d
Engelbert Paiva <engelbert.paiva@gmail.com>
2020-12-01 21:42:41 -0300

    inclusao de novos exercicios de javaScript na pasta

---
+++ arrowFunction.js
@@ -0,0 +1,21 @@
+function soma(x, y) {
+    return x + y;
+}
+
+const multiplicacao = function(x, y) {
+    return x * y;
+};
+
+const dividir = (x, y) => {
+    return x / y;
+};
+
+const objeto = () => ({nome: 'Engelbert', sobrenome: 'Paiva'});
+
+console.log(soma(3, 9));
+
+console.log(multiplicacao(5, 9));
+
+console.log(dividir(25, 5));
+
+console.log(objeto);
---
+++ template-string.js
@@ -0,0 +1,17 @@
+let meuNome = 'Engelbert';
+let meuSobrenome = 'Paiva de Sa';
+let minhaProfissao = 'Bancario';
+
+//sem template string
+console.log("Olá, eu sou " + meuNome + ' ' + meuSobrenome + ', minha profissão é: ' + minhaProfissao);
+
+//com template string
+console.log(`Olá, eu sou ${meuNome} ${meuSobrenome} e minha profissão é: ${minhaProfissao}`);
+
+console.log(`O resultado da soma de 1 + 1 = ${1 + 1}`);
+
+//console.log(`O objeto json ${{chave: 'valor'}}`);
+
+//let meuObjeto {
+//    chave: 'novo valor';
+//}
